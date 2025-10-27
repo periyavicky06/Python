af9cc8f35091a017c96fecc1161f194f4c189a47
periyavicky06 <periyavicky@gmail.com>
2025-10-27 19:06:35 +0530

    Update factor.py

--- factor.py
+++ factor.py
@@ -1,10 +1,12 @@
+
 x=int(input("Give the x value :"))
 y=int(input("Give the y value :"))
-a=min(x,y)
-factor=[]
+a= min(x,y)
+factors=[]
 for i in range(1,a+1):
     if (x%i==0) and (y%i==0):
-        factor.append(i)
+        factors.append(i)
 
-print(" ".join(map(str,factor)))
+print("common factors:"," ".join(map(str,factor)))
+
         

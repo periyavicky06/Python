1951901b326b366dd0710a24686cf6731fafd4bd
periyavicky06 <periyavicky@gmail.com>
2025-10-27 18:58:23 +0530

    Refactor Arithmetic.py for improved output and operations
    Updated arithmetic operations to include modulus and corrected print formatting.

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,9 +1,12 @@
+
 #Arithmetic operator
 a=int(input("Give the A value :"))
 b=int(input("Give the B value:"))
-print("Addition :",a+b)
-print("Subraction :",a-b)
+print(f"Addition :a+b")
+print(f"Subraction :a-b")
 print(f"Multiplication : {a*b}")
-print("Divition :",a/b)
-print("Floor Divition :",a//b)
+print(f"Divition :a/b")
+print(f"Floor Divition :a//b")
 print(f"Exponential :{a**b}")
+print(f"Modules:a%b")
+

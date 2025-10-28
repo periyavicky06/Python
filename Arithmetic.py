412dfe636adfae201cfb7cd0628a44d09544a093
periyavicky06 <periyavicky@gmail.com>
2025-10-28 09:37:35 +0530

    Refactor Arithmetic.py for improved input prompts

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,11 +1,13 @@
 
 #Arithmetic operator
-a=int(input("Give the A value :"))
-b=int(input("Give the B value:"))
-print("Addition :",a+b)
-print("Subraction :",a-b)
+a=int(input("Enter the A value :"))
+b=int(input("Enter the B value:"))
+print(f"Addition :a+b")
+print(f"Subraction :a-b")
 print(f"Multiplication : {a*b}")
-print("Divition :",a/b)
-print("Floor Divition :",a//b)
+print("Divition :a/b")
+print("Floor Divition :a//b")
 print(f"Exponential :{a**b}")
+print(f"Modules:a%b")
+
 
